6567d4d30e7b208ec792b0490faac1d09835f596
Jason Lengstorf <jason@lengstorf.com>
2023-03-07 16:32:37 -0800

    feat: step 2

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,21 @@ const container = document.querySelector(".container")!;
 const button = document.querySelector(".button")!;
 
 const endY = container.clientHeight * -1.2;
+const w = container.clientWidth;
 
 button.addEventListener("click", (event) => {
 	event.preventDefault();
 
 	const heart = heartTemplate.content.firstElementChild!.cloneNode(true);
+
+	const width = gsap.utils.random(40, 70);
+
+	const initialX = gsap.utils.random(0, w - width);
+
+	gsap.set(heart, {
+		width,
+		x: initialX,
+	});
 
 	container.appendChild(heart);
 
