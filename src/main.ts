b8eb548021957dfbbd5057e2a9138248bcb725ee
Jason Lengstorf <jason@lengstorf.com>
2023-03-07 16:28:32 -0800

    feat: step 1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
-import './style.css';
+import { gsap } from "gsap";
 
-// TODO add interactive, randomized animation
+import "./style.css";
+
+const heartTemplate: HTMLTemplateElement = document.querySelector("#heart")!;
+const container = document.querySelector(".container")!;
+const button = document.querySelector(".button")!;
+
+const endY = container.clientHeight * -1.2;
+
+button.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	const heart = heartTemplate.content.firstElementChild!.cloneNode(true);
+
+	container.appendChild(heart);
+
+	gsap.to(heart, {
+		duration: 2,
+		y: endY,
+		onComplete: () => {
+			container.removeChild(heart);
+		},
+	});
+});
